130d56d912df6db43ea3da0d7cabbd412785298d
Wensan Siegfred <wensan@local.com>
2021-08-23 22:01:45 +0800

    initial api folder

---
+++ hms_api/src/main/java/com/camotechq/hms_api/HmsApiApplication.java
@@ -0,0 +1,13 @@
+package com.camotechq.hms_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmsApiApplication.class, args);
+	}
+
+}
---
+++ hms_api/src/test/java/com/camotechq/hms_api/HmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camotechq.hms_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
